1a86e4534e7e0a2783a5338092d62ab760ed78b2
Jamie Butterworth <jamie@dpfg.net>
2020-07-28 15:36:34 +1000

    removing gulp-util

---
+++ lib/index.ts
@@ -0,0 +1,20 @@
+/*
+ * gulp-svg2png
+ *
+ * Copyright(c) 2014 - present / André König <andre.koenig@posteo.de>
+ * MIT Licensed
+ *
+ */
+
+/**
+ * @author André König <andre.koenig@posteo.de>
+ *
+ */
+
+'use strict';
+
+import SVG from './SVG';
+
+export {
+	SVG
+};
